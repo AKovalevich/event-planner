4fa8300db266c7d4ddc00fc103e86a68479c0bd4
Artem Kovalevich <kovalevich.a@i20.biz>
2017-06-04 23:56:42 +0600

    Added debug information

--- utils/db.go
+++ utils/db.go
@@ -18,10 +18,12 @@ func GetDB() (*gorm.DB, error) {
 			break
 		}
 		println("Connect to database failed. Try to reconnect to database...")
+		println("DB user: " + app.Config().DataBase.User)
+		println("DB name: " + app.Config().DataBase.Name)
 		time.Sleep(5 * time.Second)
 	}
 
-	db, err := gorm.Open("mysql", app.Config().DataBase.User+ ":@/" + app.Config().DataBase.Name + "?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", app.Config().DataBase.User + ":@/" + app.Config().DataBase.Name + "?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		return nil, err
 	}
